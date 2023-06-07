d127540f8dc5cad1d4b093c24a4be39490185793
ayush <ayush.chauhan0008@gmail.com>
2023-06-07 21:03:26 +0530

    helpful messages

--- app.py
+++ app.py
@@ -2,25 +2,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# base page
+
+
 @app.route('/')
 def index():
     return '<h1>Index Page</h1>'
+
+# domain/hello
 
 
 @app.route('/hello')
 def hello():
     return 'Hello, World'
 
+# domain/username/<your input here>
 
-@app.route('/user/<userName>')
+
+@app.route('/username/<userName>')
 def user(userName):
     return f"Hello, user: {userName}"
 
 
+# domain/user/<your integer input here>
 @app.route('/user/<int:userID>')
 def userId(userID):
     return f"Welcome, userId: {userID}"
-
 
 if __name__ == "__main__":
     app.run(debug=True)
