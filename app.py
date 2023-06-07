65d96be4f276f8e8f2e5d4022910de859708d2ec
ayush <ayush.chauhan0008@gmail.com>
2023-06-08 00:57:53 +0530

    template render and basic requests

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# base page
+
 @app.route('/')
 def index():
-    return '<h1>Index Page</h1>'
-
-# domain/hello
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
-
-# domain/username/<your input here>
-@app.route('/username/<userName>')
-def user(userName):
-    return f"Hello, user: {userName}"
-
-# domain/user/<your integer input here>
-@app.route('/user/<int:userID>')
-def userId(userID):
-    return f"Welcome, userId: {userID}"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    if "name" in request.args:
+        name = request.args["name"]
+    else:
+        name = "world"
+    return render_template('index.html', name)
